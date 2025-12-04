212c13d86e97335c1476ec8fdd9f515782f8a823
NubeEraTechnologies <nubeeratechnologies@gmail.com>
2025-12-04 15:43:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,72 +13,18 @@ terraform {
   }
 }
 
-variable "prefix" {
-  default = "devvm"
-}
-
-# ---------------------------------------------------------------------------
-# 1. REGION LIST (TEST ORDER)
-# ---------------------------------------------------------------------------
-
-locals {
-  regions = [
-    "eastus",
-    "eastus2",
-    "centralus",
-    "southcentralus",
-    "westus",
-    "westus2",
-    "westus3",
-    "northeurope",
-    "westeurope",
-    "uksouth"
-  ]
-
-  vm_sizes = [
-    "Standard_D2s_v3",
-    "Standard_D2as_v5",
-    "Standard_D2ls_v5",
-    "Standard_D2_v2"
-  ]
-}
-
-# ---------------------------------------------------------------------------
-# 2. PROBE REGION AVAILABILITY BY CREATING TEMPORARY RGs
-# ---------------------------------------------------------------------------
-
-resource "azurerm_resource_group" "probe" {
-  for_each = toset(local.regions)
-
-  name     = "${var.prefix}-probe-${each.key}"
-  location = each.key
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Choose first successfully created region
-locals {
-  working_region = keys(azurerm_resource_group.probe)[0]
-}
-
-# ---------------------------------------------------------------------------
-# 3. REAL RESOURCE GROUP (CREATED ONCE)
-# ---------------------------------------------------------------------------
+variable "prefix" { default = "devvm" }
+variable "region" {}
+variable "vm_size" {}
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-rg"
-  location = local.working_region
+  location = var.region
 }
-
-# ---------------------------------------------------------------------------
-# 4. NETWORK RESOURCES
-# ---------------------------------------------------------------------------
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
-  location            = local.working_region
+  location            = var.region
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
@@ -93,14 +39,14 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_public_ip" "public_ip" {
   name                = "${var.prefix}-pip"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = local.working_region
+  location            = var.region
   sku                 = "Standard"
   allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "nic" {
   name                = "${var.prefix}-nic"
-  location            = local.working_region
+  location            = var.region
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
@@ -111,28 +57,17 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# ---------------------------------------------------------------------------
-# 5. SSH KEY
-# ---------------------------------------------------------------------------
-
 resource "tls_private_key" "sshkey" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-# ---------------------------------------------------------------------------
-# 6. VM CREATION – TRY EACH SIZE UNTIL ONE WORKS
-# ---------------------------------------------------------------------------
-
 resource "azurerm_linux_virtual_machine" "vm" {
-  for_each = toset(local.vm_sizes)
-
   name                = "${var.prefix}-vm"
+  location            = var.region
   resource_group_name = azurerm_resource_group.rg.name
-  location            = local.working_region
-  size                = each.key
-
-  admin_username = "azureuser"
+  size                = var.vm_size
+  admin_username      = "azureuser"
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
@@ -154,38 +89,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [
-    azurerm_network_interface.nic
-  ]
 }
 
-# Choose first VM that was created
-locals {
-  working_vm_size = keys(azurerm_linux_virtual_machine.vm)[0]
-}
+output "selected_region" { value = var.region }
+output "selected_vm_size" { value = var.vm_size }
+output "public_ip" { value = azurerm_public_ip.public_ip.ip_address }
+output "private_key" { sensitive = true, value = tls_private_key.sshkey.private_key_pem }
 
-# ---------------------------------------------------------------------------
-# 7. OUTPUTS
-# ---------------------------------------------------------------------------
-
-output "region_used" {
-  value = local.working_region
-}
-
-output "vm_size_used" {
-  value = local.working_vm_size
-}
-
-output "public_ip" {
-  value = azurerm_public_ip.public_ip.ip_address
-}
-
-output "private_key" {
-  value     = tls_private_key.sshkey.private_key_pem
   sensitive = true
 }
